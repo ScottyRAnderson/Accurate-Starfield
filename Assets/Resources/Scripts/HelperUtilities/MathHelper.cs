0b171a29d0e105e3138c004de1256f10be96ba56
Scott Anderson <Scottyrichardanderson@gmail.com>
2023-06-02 11:01:34 +0100

    Initial project push

---
+++ Assets/Resources/Scripts/HelperUtilities/MathHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathHelper
+{
+    /// <summary> Normalizes a value between 0 - 1 given the minimum and maximum of the given value. E.g. Normalize(0, -1, 1) = 0.5 </summary>
+    public static float NormalizeValue(float value, float min, float max){
+        return (value - min) / (max - min);
+    }
+}
